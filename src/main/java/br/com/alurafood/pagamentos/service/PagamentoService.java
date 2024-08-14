2f6360f07549116475f1c45c05b247c1dc9cb773
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-14 09:55:47 -0300

    feat: método de criação de pagamento

--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.service;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
@@ -29,6 +30,14 @@ public class PagamentoService {
     public PagamentoDto obterPorId(Long id) {
         Pagamento pagamento = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException());
+        return mapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criarPagamento(PagamentoDto pagamentoDto) {
+        Pagamento pagamento = mapper.map(pagamentoDto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
         return mapper.map(pagamento, PagamentoDto.class);
     }
 }
