116352fe854f94eb8f4f8128b9bafc14484d948a
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-14 12:10:28 -0300

    feat: criando controller e métodos

---
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -0,0 +1,41 @@
+package br.com.alurafood.pagamentos.controller;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    public Page<PagamentoDto> listarTodos(@PageableDefault(size=10) Pageable paginacao) {
+        return service.obterTodos(paginacao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDto> listarPorId(@PathVariable @NotNull Long id) {
+        PagamentoDto pagamento = service.obterPorId(id);
+        return ResponseEntity.ok(pagamento);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDto> cadastrar(@RequestBody @Valid PagamentoDto pagamentoDto, UriComponentsBuilder uriBuilder) {
+        service.criarPagamento(pagamentoDto);
+        URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamentoDto.getId()).toUri();
+        return ResponseEntity.created(endereco).body(pagamentoDto);
+    }
+}
