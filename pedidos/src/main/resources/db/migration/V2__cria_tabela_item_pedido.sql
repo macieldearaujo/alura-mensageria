e6d04ba93a6335f07870baa0c08ea5c65c910d17
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-16 15:30:20 -0300

    fix: corrigindo a mudança da pasta

--- pedidos/src/main/resources/db/migration/V2__cria_tabela_item_pedido.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE item_do_pedido (
-    id BIGINT PRIMARY KEY,
-    descricao VARCHAR(255),
-    quantidade INTEGER NOT NULL,
-    pedido_id BIGINT NOT NULL,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id)
-);
