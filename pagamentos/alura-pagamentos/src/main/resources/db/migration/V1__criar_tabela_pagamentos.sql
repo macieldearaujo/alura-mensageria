71932e494437149fdf3c37dbed49875a37032d70
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-16 15:05:25 -0300

    fix: movendo pasta

---
+++ pagamentos/alura-pagamentos/src/main/resources/db/migration/V1__criar_tabela_pagamentos.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pagamentos (
+ id BIGSERIAL NOT NULL,
+ valor DECIMAL(19,2) NOT NULL,
+ nome VARCHAR(100) DEFAULT NULL,
+ numero VARCHAR(19) DEFAULT NULL,
+ expiracao VARCHAR(7) DEFAULT NULL,
+ codigo VARCHAR(3) DEFAULT NULL,
+ status VARCHAR(255) NOT NULL,
+ forma_de_pagamento_id BIGINT NOT NULL,
+ pedido_id BIGINT NOT NULL,
+ PRIMARY KEY (id)
+);
