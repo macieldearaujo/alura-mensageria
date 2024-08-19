9da011e1e7f0de4adaf5e52b4f50c7849cd22a8b
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-19 12:15:06 -0300

    fix: criando um gateway

--- alura-gateway/src/main/java/br/com/alurafood/gateway/GatewayApplication.java
+++ alura-gateway/src/main/java/br/com/alurafood/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package br.com.alurafood.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
