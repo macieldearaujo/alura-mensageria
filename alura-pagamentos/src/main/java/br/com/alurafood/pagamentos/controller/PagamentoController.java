3b32fdf707e9b22ec04fd772d0ae46ed049d496e
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-21 09:44:22 -0300

    feat: criando circuit breaker

--- alura-pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ alura-pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.controller;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -53,6 +54,7 @@ public class PagamentoController {
     }
 
     @PatchMapping("/{id}/confirmar")
+    @CircuitBreaker(name = "atualizaPedido", fallbackMethod = "")
     public void confirmaPagamento(@PathVariable @NotNull Long id) {
         service.confirmaPagamento(id);
     }
